88042db9c7986de28653b94cbcbbc8cc6f410324
Tzu-Han Chang <57966875+tzuhanchang@users.noreply.github.com>
2023-10-10 20:50:36 +0100

    Save model list to /tmp folder

--- madlad/utils/model.py
+++ madlad/utils/model.py
@@ -10,11 +10,12 @@ def get_model(model_name: str, model_dict: str = "/app/MG5_aMC_v2_9_16/models"):
         model_name = match.group(1)
 
     if os.path.exists(os.path.join(model_dict,model_name)) is not True:
-        get_list = subprocess.Popen(["wget","http://madgraph.phys.ucl.ac.be/models_db.dat"])
-        get_list.wait()
+        if os.path.isfile("/tmp/models_db.dat") is False:
+            get_list = subprocess.Popen(["wget","-P","/tmp/","http://madgraph.phys.ucl.ac.be/models_db.dat"])
+            get_list.wait()
 
         lookup_dict = {}
-        with open('models_db.dat', 'r') as file:
+        with open('/tmp/models_db.dat', 'r') as file:
             for line in file:
                 columns = line.strip().split()
                 if len(columns) == 2:
@@ -33,18 +34,16 @@ def get_model(model_name: str, model_dict: str = "/app/MG5_aMC_v2_9_16/models"):
         if link == "":
             raise ValueError("Model not found in the database.")
 
-        delete_list = subprocess.Popen(["rm","models_db.dat"])
-        delete_list.wait()
-
         download = subprocess.Popen(["sudo","wget","-P",model_dict+"/",link])
         download.wait()
         unzip    = subprocess.Popen(["sudo","unzip",model_dict+"/{}.zip".format(fname),"-d",model_dict])
         unzip.wait()
-        testcard = open('tmp.dat','w')
+
+        testcard = open('/tmp/convert.dat','w')
         testcard.write("""convert model %s/%s
 """%(model_dict,fname))
         testcard.close()
-        convert = subprocess.Popen(["sudo", "/usr/local/bin/mg5", "tmp.dat"])
+        convert = subprocess.Popen(["sudo", "/usr/local/bin/mg5", "/tmp/convert.dat"])
         convert.wait()
-        cleanup = subprocess.Popen(["rm", "tmp.dat"])
+        cleanup = subprocess.Popen(["rm", "/tmp/convert.dat"])
         cleanup.wait()
