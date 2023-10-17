b408922de1c909154a58e088f3d3232a66681b6f
tzuhanchang <han.tzu.zhang@gmail.com>
2023-10-17 08:55:38 -0500

    Fix TypeError

--- madlad/utils/singularity.py
+++ madlad/utils/singularity.py
@@ -5,16 +5,16 @@ from madlad.utils import get_pdfset_singularity, get_model_singularity
 
 def create_singularity_build(pdf_id: int, model_name: str, repo: str):
     r"""Create a Singularity build file.
-    
+
     Args:
         pdf_id (int): LHAPDF ID.
         model_name (str): model name.
     """
     with open("singularity", "w") as f:
         f.write("""Bootstrap: docker
-From: %s
+From: """+repo+"""
 
-%post"""%(repo)+get_pdfset_singularity(pdf_id)+get_model_singularity(model_name))
+%post"""+get_pdfset_singularity(pdf_id)+get_model_singularity(model_name))
 
 
 def build_sif(pdf_id: int, model_name: str, repo: str = "tzuhanchang/madlad:amd64"):
