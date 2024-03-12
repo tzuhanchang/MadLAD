437dcd4aacfd0713abb8cf2deb3269722c4a8cb0
Tzu-Han Chang <57966875+tzuhanchang@users.noreply.github.com>
2024-03-12 21:17:17 +0000

    Move container checks to madlad.container

--- madlad/utils/__init__.py
+++ madlad/utils/__init__.py
@@ -1,8 +1,5 @@
-from .container import is_running_in_docker_container, is_running_in_singularity_container
 from .settings import config
 
 __all__ = [
-    'is_running_in_docker_container',
-    'is_running_in_singularity_container',
     'config'
 ]
