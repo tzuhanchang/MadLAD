9779063bbc961151722f5bfd881b67ebd9fac3be
Tzu-Han Chang <57966875+tzuhanchang@users.noreply.github.com>
2024-03-27 02:28:57 +0000

    Implement `post` (#33)

--- madlad/controls/cleanup.py
+++ madlad/controls/cleanup.py
@@ -12,6 +12,9 @@ def cleanUp(cfg : DictConfig, logger) -> None:
 
     if os.path.exists(f"mg5_exec_card-{os.path.basename(cfg['gen']['block_model']['save_dir'])}"):
         subprocess.Popen(["rm", f"mg5_exec_card-{os.path.basename(cfg['gen']['block_model']['save_dir'])}"])
-    
+
+    if os.path.exists("post-commands"):
+        subprocess.Popen(["rm", "post-commands"])
+
     if os.path.exists("py.py"):
         subprocess.Popen(["rm", "py.py"])
