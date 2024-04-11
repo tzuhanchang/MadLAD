dcb904b60e2ae9396f9ec658732dd3c6a57e9d6b
Tzu-Han Chang <57966875+tzuhanchang@users.noreply.github.com>
2024-04-11 18:46:08 +0100

    update

--- madlad/controls/delphes.py
+++ madlad/controls/delphes.py
@@ -12,9 +12,20 @@ def runDelphes(cfg : DictConfig, dir: str, run_with: str, image_name: str, logge
     if 'block_delphes' in list(cfg['gen'].keys()):
 
         logger.info("Running detector simulation with Delphes.")
+        evt_dir = 'run_01_decayed_1' if 'block_madspin' in list(cfg['gen'].keys()) else 'run_01'
+
+        shower = True if 'shower' in list(cfg['run'].keys()) and cfg['run']['shower'] is True else False
+        if cfg['gen']['block_model']['order'] == "nlo" or 'order' not in list(cfg['gen']['block_model']):
+            file_name = 'events_PYTHIA8_0.hepmc' if shower else 'events.lhe'
+        else:
+            file_name = 'tag_1_pythia8_events.hepmc' if shower else 'unweighted_events.lhe'
+
         ecard = open(f"delphes_exec_card-{os.path.basename(dir)}","w")
-        ecard.write("gunzip Output/Events/run_01_decayed_1/events_PYTHIA8_0.hepmc.gz \n")
-        ecard.write(f"DelphesHepMC2 delphes/{cfg['gen']['block_delphes']['delphes_card']} {dir}.root {dir}/Events/run_01_decayed_1/events_PYTHIA8_0.hepmc")
+        ecard.write(f"gunzip {dir}/Events/{evt_dir}/{file_name}+'.gz' \n")
+        if shower:
+            ecard.write(f"DelphesHepMC2 delphes/{cfg['gen']['block_delphes']['delphes_card']} {dir}.root {dir}/Events/{evt_dir}/{file_name}")
+        else:
+            ecard.write(f"DelphesLHEF delphes/{cfg['gen']['block_delphes']['delphes_card']} {dir}.root {dir}/Events/{evt_dir}/{file_name}")
         ecard.close()
 
         if run_with == "docker":
