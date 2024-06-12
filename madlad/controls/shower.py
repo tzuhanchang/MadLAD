17faabd91fcd13166361ccd6f42c56793c2490d3
Tzu-Han Chang <57966875+tzuhanchang@users.noreply.github.com>
2024-06-12 13:18:15 +0100

    Update

--- madlad/controls/shower.py
+++ madlad/controls/shower.py
@@ -10,32 +10,31 @@ from omegaconf import DictConfig
 def runShower(cfg : DictConfig, dir: str, run_with: str, image_name: str, logger) -> None:
     r"""Launch parton shower for LO events.
     """
-    if 'post' in list(cfg.keys()):
-        image_name, run_with = checkImage(cfg, logger)
+    image_name, run_with = checkImage(cfg, logger)
 
-        logger.info('Writting LO parton shower commands.')
-        evt_dir = 'run_01_decayed_1' if 'block_madspin' in list(cfg['gen'].keys()) else 'run_01'
-        ecard = open(f"mg5_lo_shower_card-{os.path.basename(dir)}","w")
-        ecard.write(f"launch {dir} -i\nshower {evt_dir}")
-        ecard.close()
+    logger.info('Writting LO parton shower commands.')
+    evt_dir = 'run_01_decayed_1' if 'block_madspin' in list(cfg['gen'].keys()) else 'run_01'
+    ecard = open(f"mg5_lo_shower_card-{os.path.basename(dir)}","w")
+    ecard.write(f"launch {dir} -i\nshower {evt_dir}")
+    ecard.close()
 
-        if run_with == "docker":
-            logger.info('Running parton shower for LO events using Docker.')
-            subprocess.run(
-                [
-                    "docker", "run", "--rm", "-it", "-w", "/home/atreus/data",
-                    "-v", f"{Path().absolute()}:/home/atreus/data",
-                    image_name, cfg['run']['mg5'],
-                    f"mg5_lo_shower_card-{os.path.basename(dir)}"
-                ]
-            )
+    if run_with == "docker":
+        logger.info('Running parton shower for LO events using Docker.')
+        subprocess.run(
+            [
+                "docker", "run", "--rm", "-it", "-w", "/home/atreus/data",
+                "-v", f"{Path().absolute()}:/home/atreus/data",
+                image_name, cfg['run']['mg5'],
+                f"mg5_lo_shower_card-{os.path.basename(dir)}"
+            ]
+        )
 
-        if run_with == "singularity":
-            logger.info('Running parton shower for LO events using Singularity.')
-            subprocess.run(
-                [
-                    "singularity", "exec", "--bind", f"{Path().absolute()}",
-                    image_name, cfg['run']['mg5'],
-                    f"mg5_lo_shower_card-{os.path.basename(dir)}"
-                ]
-            )
+    if run_with == "singularity":
+        logger.info('Running parton shower for LO events using Singularity.')
+        subprocess.run(
+            [
+                "singularity", "exec", "--bind", f"{Path().absolute()}",
+                image_name, cfg['run']['mg5'],
+                f"mg5_lo_shower_card-{os.path.basename(dir)}"
+            ]
+        )
