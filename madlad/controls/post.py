f1a339655290c290205bf997dae58096fb80b352
Tzu-Han Chang <57966875+tzuhanchang@users.noreply.github.com>
2025-11-24 21:09:57 +0000

    Add pre-launch commands

--- madlad/controls/post.py
+++ madlad/controls/post.py
@@ -8,6 +8,38 @@ from madlad.container import checkImage
 from omegaconf import DictConfig
 
 
+def runPreLaunch(cfg : DictConfig, logger) -> None:
+    r"""Launch event generation.
+    """
+    if 'pre-launch' in list(cfg.keys()):
+        image_name, run_with = checkImage(cfg, logger)
+
+        logger.info('Writting pre-launch commands.')
+        ecard = open("pre-launch-commands","w")
+        for command in cfg['pre-launch']:
+            ecard.write(str(command)+'\n')
+        ecard.close()
+
+        if run_with == "docker":
+            logger.info('Running pre-launch commands using Docker.')
+            subprocess.run(
+                [
+                    "docker", "run", "--rm", "-it", "-w", "/root",
+                    "-v", f"{Path().absolute()}:/root",
+                    image_name, "/bin/bash", f"pre-launch-commands"
+                ]
+            )
+
+        if run_with == "singularity":
+            logger.info('Running pre-launch commands using Singularity.')
+            subprocess.run(
+                [
+                    "singularity", "exec", "--bind", f"{Path().absolute()}",
+                    image_name, "/bin/bash", f"pre-launch-commands"
+                ]
+            )
+
+
 def runPost(cfg : DictConfig, logger) -> None:
     r"""Launch event generation.
     """
